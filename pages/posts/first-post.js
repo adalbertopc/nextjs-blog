a87a920c6a1266a2bf1db5ba923aef8aac1c835c
Jesús Adalberto Prado Campas <jesus.prado@MacBook-Pro-de-admin.local>
2022-02-24 13:15:10 -0700

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+import Layout from "../../components/layout";
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
